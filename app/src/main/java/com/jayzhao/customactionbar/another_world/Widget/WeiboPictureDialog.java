46cf8937e0f01999d4cf50426163f4668ffe09bf
Zhao Jiabao <zhaojiabao@smartdevices.com.cn>
2016-08-19 16:14:51 +0800

    修复 PictureView在转屏时出现的崩溃问题

--- app/src/main/java/com/jayzhao/customactionbar/another_world/Widget/WeiboPictureDialog.java
+++ app/src/main/java/com/jayzhao/customactionbar/another_world/Widget/WeiboPictureDialog.java
@@ -4,6 +4,7 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,16 +17,22 @@ import com.jayzhao.customactionbar.R;
  */
 public class WeiboPictureDialog extends DialogFragment {
     private LoadingPictureView loadingPictureView = null;
+    private ValueAnimator mAnimator = null;
+
+    private static final String TAG = "WeiboPictureDialog";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.picture_loading, container, false);
         loadingPictureView = (LoadingPictureView) view.findViewById(R.id.weibo_picture_view);
-        ValueAnimator animator = ValueAnimator.ofInt(0, 360);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(5000);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        mAnimator = ValueAnimator.ofInt(0, 360);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setDuration(5000);
+        /**
+         * 这里是否已经出现了内存泄露？
+         */
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 loadingPictureView.setProgress((int) animation.getAnimatedValue());
@@ -34,7 +41,7 @@ public class WeiboPictureDialog extends DialogFragment {
                 }
             }
         });
-        animator.start();
+        mAnimator.start();
         return view;
     }
 
@@ -44,5 +51,21 @@ public class WeiboPictureDialog extends DialogFragment {
         int style = DialogFragment.STYLE_NO_FRAME;
         int theme = R.style.WeiboLoadingDialog;
         setStyle(style, theme);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+        /**
+         * 这段代码可以修复一个bug:
+         * 当屏幕发生旋转时，DialogFragment会被销毁，并重建。但是销毁之前的mAnimator还在，当mAnimator结束后，
+         * 去dismiss这个DialogFragment时，会发现，原先的DialogFragment已经是null了
+         * 所以需要在原先DialogFragment销毁（onDestroy）时把这个动画取消掉
+         */
+        if(mAnimator != null) {
+            mAnimator.cancel();
+            mAnimator = null;
+        }
     }
 }

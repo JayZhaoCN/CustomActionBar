7943a6ade07334b15d2fbd5519abaa1f13317db8
Zhao Jiabao <zhaojiabao@smartdevices.com.cn>
2016-08-25 20:51:52 +0800

    完善自定义字体TextView代码

--- app/src/main/java/com/jayzhao/customactionbar/another_world/Widget/TypefaceTextView.java
+++ app/src/main/java/com/jayzhao/customactionbar/another_world/Widget/TypefaceTextView.java
@@ -9,15 +9,38 @@ import android.widget.TextView;
 
 /**
  * Created by Jay on 16-8-25.
+ * 可以自定义字体的TextView.
+ * 用法：
+ * 在layout xml文件中增加
+ * android:fontFamily="miui_light"
+ * android:fontFamily="miui_bold"
+ * android:fontFamily="miui_normal"
+ * android:fontFamily="din_med"
+ * android:fontFamily="pt_din"
+ * 共五种
  */
 public class TypefaceTextView extends TextView {
     private static final String TAG = "TypefaceTextView";
 
-    public static final String MIUI_BOLD = "miui_bold";
-    public static final String MIUI_LIGHT = "miui_light";
-    public static final String MIUI_NORMAL = "miui_normal";
-    public static final String PT_DIN = "pt_din";
-    public static final String DIN_MED = "din_med";
+    /**
+     * 字体枚举
+     */
+    public enum Font {
+        MIUI_BOLD("miui_bold", "fonts/MIUI-Bold.ttf"),
+        MIUI_LIGHT("miui_light", "fonts/MIUI-Light.ttf"),
+        MIUI_NORMAL("miui_normal", "fonts/MIUI-Normal.ttf"),
+        PT_DIN("pt_din", "fonts/pt_din_condensed_cyrillic.ttf"),
+        DIN_MED("din_med", "fonts/dincond_medium.otf");
+
+        private String mFontName = null;
+        private String mFontPath = null;
+
+        Font(String fontName, String fontPath) {
+            this.mFontName = fontName;
+            this.mFontPath = fontPath;
+        }
+    }
+
 
     public TypefaceTextView(Context context) {
         this(context, null);
@@ -38,18 +61,18 @@ public class TypefaceTextView extends TextView {
         String typeFacePath = null;
         if (typeFaceStr == null) {
             return;
-        } else if (typeFaceStr.equals(MIUI_BOLD)) {
-            typeFacePath = "fonts/MIUI-Bold.ttf";
-        } else if (typeFaceStr.equals(MIUI_LIGHT)) {
-            typeFacePath = "fonts/MIUI-Light.ttf";
-        } else if (typeFaceStr.equals(MIUI_NORMAL)) {
-            typeFacePath = "fonts/MIUI-Normal.ttf";
-        } else if (typeFaceStr.equals(PT_DIN)) {
-            typeFacePath = "fonts/pt_din_condensed_cyrillic.ttf";
-        } else if (typeFaceStr.equals(DIN_MED)) {
-            typeFacePath = "fonts/dincond_medium.otf";
+        } else if (typeFaceStr.equals(Font.MIUI_BOLD.mFontName)) {
+            typeFacePath = Font.MIUI_BOLD.mFontPath;
+        } else if (typeFaceStr.equals(Font.MIUI_LIGHT.mFontName)) {
+            typeFacePath = Font.MIUI_LIGHT.mFontPath;
+        } else if (typeFaceStr.equals(Font.MIUI_NORMAL.mFontName)) {
+            typeFacePath = Font.MIUI_LIGHT.mFontPath;
+        } else if (typeFaceStr.equals(Font.PT_DIN.mFontName)) {
+            typeFacePath = Font.PT_DIN.mFontPath;
+        } else if (typeFaceStr.equals(Font.DIN_MED.mFontName)) {
+            typeFacePath = Font.DIN_MED.mFontPath;
         }
         Log.i(TAG, "typeFacePath: " + typeFacePath);
-        super.setTypeface(Typeface.createFromAsset(context.getAssets(), typeFacePath));
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), typeFacePath));
     }
 }
